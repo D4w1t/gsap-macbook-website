40b3419e561eccea567865782e34fbc7128e7d32
Dawit Nigussie <dawit9n@gmail.com>
2025-10-31 19:24:48 +0300

    Refactor Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
+// Set base dynamically for local dev and GitHub Pages
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/gsap-macbook-website/',
+  base: process.env.NODE_ENV === 'production' ? '/gsap-macbook-website/' : '/',
 });
+
